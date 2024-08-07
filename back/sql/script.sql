d275b832c26247bf70e6c1cfa67f2f208ced912f
Miandritiana-Generis <miandritiana.a@generis.mg>
2024-08-07 13:53:29 +0300

    id

--- back/sql/script.sql
+++ back/sql/script.sql
@@ -1,5 +1,5 @@
 CREATE TABLE personne (
-    idPersonne SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100),
     prenom VARCHAR(100),
     genre VARCHAR(50),
@@ -10,35 +10,35 @@ CREATE TABLE personne (
 
 -- enseignant
 CREATE TABLE enseignant (
-    idEnseignant SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     idPersonne INTEGER REFERENCES personne(idPersonne)
 );
 
 CREATE TABLE niveau(
-    idNiveau SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE filiere(
-    idFiliere SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE matiere(
-    idMatiere SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     idNiveau INTEGER REFERENCES niveau(idNiveau),
     idFiliere INTEGER REFERENCES filiere(idFiliere)
 );
 
 CREATE TABLE enseignant_matiere(
-    idEnseignant_matiere SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     idEnseignant INTEGER REFERENCES enseignant(idEnseignant),
     idMatiere INTEGER REFERENCES matiere(idMatiere)
 );
 
 CREATE TABLE classe(
-    idClasse SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     idFiliere INTEGER REFERENCES filiere(idFiliere),
     idNiveau INTEGER REFERENCES niveau(idNiveau)
@@ -46,20 +46,20 @@ CREATE TABLE classe(
 
 -- etudiant
 CREATE TABLE etudiant(
-    idEtudiant SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     idPersonne INTEGER REFERENCES personne(idPersonne),
     idPere INTEGER REFERENCES personne(idPersonne),
     idMere INTEGER REFERENCES personne(idPersonne)
 );
 
 CREATE TABLE annee_scolaire(
-    idAS SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     anneeD INTEGER NOT NULL,
     anneeF INTEGER NOT NULL
 );
 
 CREATE TABLE classe_etudiant(
-    idClasse_etudiant SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     idClasse INTEGER REFERENCES classe(idClasse),
     idEtudiant INTEGER REFERENCES etudiant(idEtudiant),
     idAnnee_scolaire INTEGER REFERENCES annee_scolaire(idAS)
@@ -67,25 +67,25 @@ CREATE TABLE classe_etudiant(
 
 -- personnel
 CREATE TABLE direction(
-    idDirection SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     numCode VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE pat(
-    idPat SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     idPersonne INTEGER REFERENCES personne(idPersonne),
     idDirection INTEGER REFERENCES direction(idDirection)
 );
 
 -- ET
 CREATE TABLE salle(
-    idSalle SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE et(
-    idEt SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     debut TIME NOT NULL,
     fin TIME NOT NULL,
