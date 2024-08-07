371e54f9f7e884575644b3e96d9fee1fdd90a784
Miandritiana-Generis <miandritiana.a@generis.mg>
2024-08-07 17:34:41 +0300

    script

--- back/sql/view.sql
+++ back/sql/view.sql
@@ -1,4 +1,4 @@
-CREATE VIEW v_ET_now AS
+CREATE VIEW v_ET_now_week AS
 SELECT
     EXTRACT(YEAR FROM e.date) AS year,
     EXTRACT(WEEK FROM e.date) AS week,
@@ -6,18 +6,24 @@ SELECT
     e.date,
     e.debut,
     e.fin,
-    c.nom AS classe_nom,
-    m.nom AS matiere_nom,
-    s.nom AS salle_nom,
+    c.nom AS classe,
+    pens.nom AS ens_nom,
+    pens.prenom AS ens_prenom,
+    m.nom AS matiere,
+    s.nom AS salle,
     e.estAnnule
 FROM
     et e
 JOIN
-    classe c ON e.idClasse = c.idClasse
+    classe c ON e.idClasse = c.id
 JOIN
-    matiere m ON e.idMatiere = m.idMatiere
+    enseignant ens ON e.idEnseignant = ens.id
 JOIN
-    salle s ON e.idSalle = s.idSalle
+    personne pens ON ens.idPersonne = pens.id
+JOIN
+    matiere m ON e.idMatiere = m.id
+JOIN
+    salle s ON e.idSalle = s.id
 WHERE
     EXTRACT(YEAR FROM e.date) = EXTRACT(YEAR FROM CURRENT_DATE)
     AND EXTRACT(WEEK FROM e.date) = EXTRACT(WEEK FROM CURRENT_DATE)
