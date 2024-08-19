0a765302694d3695e78f9350f1d8a905f3071fc0
Miandritiana-Generis <miandritiana.a@generis.mg>
2024-08-19 17:20:50 +0300

    tsy mety le assets import ena

--- front/angular/src/app/app.routes.ts
+++ front/angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SalleComponent } from './salle/salle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'salle',
+        component: SalleComponent
+    }
+];
