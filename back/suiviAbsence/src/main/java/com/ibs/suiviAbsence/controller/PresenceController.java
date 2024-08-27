32143bbf7478424b1cff8af53b81d04117d70646
priscamamisoageneris <prisca.m@generis.mg>
2024-08-27 09:06:58 +0300

    Create PresenceController.java

---
+++ back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/controller/PresenceController.java
@@ -0,0 +1,11 @@
+package com.ibs.suiviAbsence.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("presences")
+public class PresenceController {
+
+}
