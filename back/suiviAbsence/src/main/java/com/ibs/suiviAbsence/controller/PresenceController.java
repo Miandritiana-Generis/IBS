f14f7745a9ea154d9affc62763b9ecc6ceab4b14
priscamamisoageneris <prisca.m@generis.mg>
2024-08-27 16:37:59 +0300

    Update PresenceController.java

--- back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/controller/PresenceController.java
+++ back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/controller/PresenceController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibs.suiviAbsence.modele.DetailPresence;
+
 
 @RestController
 @RequestMapping("presences")
 public class PresenceController {
 
     @PostMapping
-    public ResponseEntity insert(@RequestBody ){
+    public ResponseEntity insert(@RequestBody DetailPresence detailPresence){
         ResponseEntity reponse=null;
 
         return reponse;
