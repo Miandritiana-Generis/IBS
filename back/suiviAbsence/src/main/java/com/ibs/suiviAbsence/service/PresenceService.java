9289894d396fea4cc1331de01d03aafc3317e38f
priscamamisoageneris <prisca.m@generis.mg>
2024-08-27 15:58:09 +0300

    Update PresenceService.java

--- back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/service/PresenceService.java
+++ back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/service/PresenceService.java
@@ -3,6 +3,7 @@ package com.ibs.suiviAbsence.service;
 import java.sql.Date;
 import java.sql.Time;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,13 +17,18 @@ public class PresenceService {
     ViewEdtAllInfoRepository viewEdtAllInfoRepository;
     public ViewEdtAllInfo getInfoEdt(int idSalle,int idEdt){
         ViewEdtAllInfo edt=null;
-        if(idEdt==0){
-            edt=viewEdtAllInfoRepository.findById(idEdt).get();
+        if(idEdt!=0){
+            Optional<ViewEdtAllInfo> optional=viewEdtAllInfoRepository.findById(idEdt);
+            if(optional.isPresent()){
+                edt=viewEdtAllInfoRepository.findById(idEdt).get();
+            }
+            
         }
         else{
             long currentTimeMillis=System.currentTimeMillis();
             Date date= new Date(currentTimeMillis);
             Time time = new Time(currentTimeMillis);
+            
             List<ViewEdtAllInfo> liste= viewEdtAllInfoRepository.findAllByIdSalle(idSalle, date, time);
             if(!liste.isEmpty()){
                 edt=liste.get(0);
