7887abaf5b8552095f4c09162e381c7961e50d4f
Narindraaaaa <narindra.r@generis.mg>
2024-09-03 16:07:01 +0300

    back - valider prof et delegue

--- back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/repository/PresenceRepository.java
+++ back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/repository/PresenceRepository.java
@@ -1,9 +1,24 @@
 package com.ibs.suiviAbsence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ibs.suiviAbsence.modele.Presence;
 
+import jakarta.transaction.Transactional;
+
 public interface PresenceRepository extends JpaRepository<Presence, Integer>{
     public Presence findAllByIdEdt(int edt);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Presence p SET p.valideProf = 1 WHERE p.idEdt = :idEdt")
+    public void validerFichePresence(@Param("idEdt") int idEdt);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Presence p SET p.valideDelegue = 1 WHERE p.idEdt = :idEdt")
+    public void validerFichePresenceDelegue(@Param("idEdt") int idEdt);
 }
--- back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/service/PresenceService.java
+++ back/suiviAbsence/src/main/java/com/ibs/suiviAbsence/service/PresenceService.java
@@ -4,12 +4,15 @@ import java.sql.Date;
 import java.sql.Time;
 import java.util.List;
 import java.util.Optional;
+import java.time.LocalTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ibs.suiviAbsence.modele.Edt;
 import com.ibs.suiviAbsence.modele.Presence;
 import com.ibs.suiviAbsence.modele.ViewEdtAllInfo;
+import com.ibs.suiviAbsence.repository.EdtRepository;
 import com.ibs.suiviAbsence.repository.PresenceRepository;
 import com.ibs.suiviAbsence.repository.ViewEdtAllInfoRepository;
 
@@ -19,6 +22,8 @@ public class PresenceService {
     ViewEdtAllInfoRepository viewEdtAllInfoRepository;
     @Autowired
     PresenceRepository presenceRepository;
+    @Autowired
+    EdtRepository edtRepository;
 
     public Presence recupererPresence(int idEdt){
         Presence presence=presenceRepository.findAllByIdEdt(idEdt);
@@ -58,4 +63,55 @@ public class PresenceService {
         return edt;
     }
 
+    /**
+     * Cette metier permet de valider le fiche de presence en tant que prof mais ne fiche ne doit être
+     * validable que 30 min après le cours
+     * @param idSalle
+     * @param idEdt
+     * @return
+     */
+    public void validerProf(Integer idEdt) {
+        Optional<Edt> edt = edtRepository.findById(idEdt);        
+        if (edt.isPresent()) {
+            Time heureDebut = edt.get().getDebut();
+            Time heureFin = edt.get().getFin();
+            LocalTime currentTime = LocalTime.now();
+
+            
+            LocalTime debutLocalTime = heureDebut.toLocalTime();
+            LocalTime finLocalTime = heureFin.toLocalTime();
+
+            
+            LocalTime debutPlus30Min = debutLocalTime.plusMinutes(30);
+
+            
+            if (currentTime.isAfter(debutPlus30Min) && currentTime.isBefore(finLocalTime)) {
+                presenceRepository.validerFichePresence(idEdt);
+                System.out.println("L'heure actuelle est entre l'heure de début ajustée et l'heure de fin.");
+            } else {
+                System.out.println("L'heure actuelle n'est pas entre l'heure de début ajustée et l'heure de fin.");
+            }
+        } else {
+            System.out.println("Aucune entrée trouvée pour l'ID fourni.");
+        }
+    }
+
+    /**
+     * Cette metier permet de valider le fiche de presence en tant que prof mais ne fiche ne doit être
+     * validable que 30 min après le cours
+     * @param idSalle
+     * @param idEdt
+     * @return
+     */
+    public void validerDelegue(Integer idEdt) {
+        Optional<Presence> presence = presenceRepository.findById(idEdt);
+        if (presence.get().getValideProf()==1) {
+            presenceRepository.validerFichePresenceDelegue(idEdt);
+        } 
+        else {
+            System.out.println("Vous ne pouvez pas encore valider le fiche de presence");
+        }
+    }
 }
+
+
