e228639daf968ed0a79d52ba8713b5a7ff885d6e
Miandritiana-Generis <miandritiana.a@generis.mg>
2024-09-11 10:13:52 +0300

    notification ts

--- front/package/src/app/layouts/full/header/header.component.ts
+++ front/package/src/app/layouts/full/header/header.component.ts
@@ -8,7 +8,11 @@ import {
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
-
+import { Notification } from 'src/app/modeles/Notification';
+import { NotificationEdtService } from 'src/app/services/notification-edt.service';
+import 'moment/locale/fr'; 
+import * as moment from 'moment';
+import 'moment/locale/fr'; 
 
 @Component({
   selector: 'app-header',
@@ -24,13 +28,36 @@ export class HeaderComponent {
   @Output() toggleCollapsed = new EventEmitter<void>();
 
   showFiller = false;
-  nom="My profil"
+  nom="My profil";
+  count=0;
+  page =0;
+  notification:Notification[]=[];
   constructor(public dialog: MatDialog,private authService:AuthService ,
-    private router:Router
+    private router:Router,
+    private notificationService:NotificationEdtService
   ) {
     this.nom=localStorage.getItem("nom")|| "My Profil";
+    this.setNotification();
   }
-
+  public charger(event: MouseEvent){
+    event.stopPropagation();
+    this.page=this.page+1;
+    this.setNotification();
+  }
+  public actualiser(event: MouseEvent){
+    this.notification=[];
+    event.stopPropagation();
+    this.setNotification();
+  }
+  public setNotification(){
+    this.notificationService.getNotification(this.page).subscribe(
+      (data: any)=> {
+        this.count=data.count;
+        for(let item of data.data){
+          this.notification.push(item)
+        }
+      });
+  }
   logout(){
     this.authService.logout();
     this.router.navigate(['/authentication/login']);
@@ -44,7 +71,22 @@ export class HeaderComponent {
     return time.slice(0, 5);
   }
 
-  redirifer(idEdt:number){
-    this.router.navigate(['/fiche-presence'], { queryParams: { id_edt: idEdt } });
+  redirifer(idEdt:number , type:number){
+    if(type==1){
+      this.router.navigate(['/fiche-presence'], { queryParams: { id_edt: idEdt } });
+    }
   }
+
+  public formaterDate(date : Date): string {
+    // Vérifie que this.date n'est pas undefined ou null
+    if (!date) {
+        return '';  // Retourne une chaîne vide si la date est absente
+    }
+
+    // Créer un objet moment à partir de this.date
+    const dateTemp = moment(date);
+
+    // Formatage de la date en utilisant moment
+    return dateTemp.format('dddd D MMMM YYYY');
+}
 }
